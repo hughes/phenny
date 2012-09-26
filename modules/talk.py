be65db4453c24df87db1a0b5343b2eb6857b3940
Matt Hughes <hughesm02@gmail.com>
2012-09-26 13:04:01 -0400

    respond to new talk type

--- modules/talk.py
+++ modules/talk.py
@@ -1,8 +1,19 @@
 import cleverbot
 mycb = cleverbot.Session()
+
+def speak(request):
+    return mycb.Ask(request)
+
 def talk(phenny, input):
-	result = mycb.Ask(input[11:])
-        phenny.reply(result)
+    phenny.reply(speak(input[11:]))
+
 talk.rule = r'$nickname:'
 talk.priority = 'low'
 talk.thread = False
+
+def talk2(phenny, input):
+    phenny.reply(speak(input[11:]))
+
+talk2.rule = r'$nickname,'
+talk2.priority = 'low'
+talk2.thread = False
