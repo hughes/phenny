14da6002c6e3d0034550ceda557ceab6626ab350
Matt Hughes <hughesm02@gmail.com>
2012-09-26 13:07:29 -0400

    foo

--- modules/ping.py
+++ modules/ping.py
@@ -2,3 +2,8 @@ def ping(phenny, input):
    phenny.say('pong')
 ping.commands=['ping']
 ping.priority='medium''
+
+def foo(phenny, input):
+   phenny.say('bar')
+ping.commands=['foo']
+ping.priority='medium''
