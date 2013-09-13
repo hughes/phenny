40f1faccb55fc17751142e6d1a14eeee27c9b3e2
Matt Hughes <hughesm02@gmail.com>
2013-09-13 16:38:27 -0400

    replaced talk with Eugene :)

---
+++ modules/eugene.py
@@ -0,0 +1,53 @@
+import os
+import simplejson
+import requests
+
+dotdir = os.path.expanduser('~/.phenny')
+cookie_file = dotdir+'/eugene.json'
+
+EUGENE_URL = "http://www.princetonai.com/bot/botanswer.do"
+
+def load_cookies():
+    try:
+        f = open(cookie_file)
+        cookies = simplejson.loads(f.read())
+        f.close()
+        return cookies
+    except:
+        return {}
+
+def save_cookies(cookies):
+    try:
+        f = open(cookie_file, 'w')
+        f.write(simplejson.dumps(cookies, indent=2))
+        f.close()
+        return True
+    except:
+        return False
+
+def get_cookie(username):
+    cookies = load_cookies()
+    if username in cookies:
+        return {"JSESSIONID": cookies.get(username)}
+    else:
+        return {}
+
+def set_cookie(username, cookie):
+    cookies = load_cookies()
+    cookies[username] = cookie
+    save_cookies(cookies)
+
+def eugene(phenny, input):
+    cookies = get_cookie(input.nick)
+    payload = {"request": input[11:]}
+
+    r = requests.get(EUGENE_URL, params=payload, cookies=cookies)
+
+    if len(r.cookies):
+        set_cookie(input.nick, r.cookies['JSESSIONID'])
+
+    phenny.reply(r.text)
+
+eugene.rule = r'$nickname:?'
+eugene.priority = 'low'
+eugene.thread = False
--- modules/talk.py
+++ modules/talk.py
@@ -1,19 +1,19 @@
-import cleverbot
-mycb = cleverbot.Session()
+# import cleverbot
+# mycb = cleverbot.Session()
 
-def speak(request):
-    return mycb.Ask(request)
+# def speak(request):
+#     return mycb.Ask(request)
 
-def talk(phenny, input):
-    phenny.reply(speak(input[11:]))
+# def talk(phenny, input):
+#     phenny.reply(speak(input[11:]))
 
-talk.rule = r'$nickname:'
-talk.priority = 'low'
-talk.thread = False
+# talk.rule = r'$nickname:'
+# talk.priority = 'low'
+# talk.thread = False
 
-def talk2(phenny, input):
-    phenny.reply(speak(input[11:]))
+# def talk2(phenny, input):
+#     phenny.reply(speak(input[11:]))
 
-talk2.rule = r'$nickname,'
-talk2.priority = 'low'
-talk2.thread = False
+# talk2.rule = r'$nickname,'
+# talk2.priority = 'low'
+# talk2.thread = False
